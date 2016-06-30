71f9caedf38e82f33d66321c32a7bf52db6fa5e9
slam24 <luismartinez242011@gmail.com>
2016-06-29 23:01:16 -0600

    feat: Form & create mascota

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 
-from apps.mascota.views import index
+from apps.mascota.views import index, mascota_view
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', index, name='index'),
+    url(r'^nuevo', mascota_view, name='mascota_crear'),
 ]
